f731abf6d2ba4696f02199a3376a0812e7b71d1d
Alunos <eduardacristinasilva2008@gmail.com>
2024-11-25 16:37:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using SistemaBancario;
+using System.Drawing;
+
+ContaCorrente pessoa1= new ContaCorrente { Titular="Teste", Saldo=500,LimiteCredito=300};
+ContaPoupanca pessoa2= new ContaPoupanca { Titular="Teste2", Saldo=500, TaxaJuros=0.5};
+
